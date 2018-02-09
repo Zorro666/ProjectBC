3e08b13187dce0ea0282a446806229590f43a012
Jake Turner <jake@unity3d.com>
2018-02-09 20:09:44 +0100

    Moved Start() -> Initialise() Start() is not called by hot reload

--- Assets/RaceUI.cs
+++ Assets/RaceUI.cs
@@ -21,46 +21,6 @@ public class RaceUI : MonoBehaviour
         m_playedCardsValue = new Text [GameLogic.PlayerCount, NumberOfCubes];
     }
 
-    void Start ()
-    {
-        var raceCardName = "/" + name + "/RaceCard/";
-        var backgroundName = raceCardName + "Background";
-        var backgroundGO = GameObject.Find (backgroundName);
-        m_background = backgroundGO.GetComponent<Image> ();
-        if (m_background == null)
-            Debug.LogError ("Can't find Background " + backgroundName);
-        var cubeNamePrefix = backgroundName + "/";
-        for (int i = 0; i < NumberOfCubes; ++i) {
-            var cubeIndex = i + 1;
-            var cubeName = cubeNamePrefix + "Cube" + cubeIndex;
-            var cubeGO = GameObject.Find (cubeName);
-            m_cubeImages [i] = cubeGO.GetComponent<Image> ();
-            if (m_cubeImages [i] == null)
-                Debug.LogError ("Can't find Cube[" + cubeIndex + "] '" + cubeName + "'");
-        }
-
-        for (int player = 0; player < GameLogic.PlayerCount; ++player) {
-            var playedCardsRootName = raceCardName + (Player)player + "Card";
-            for (int j = 0; j < NumberOfCubes; ++j) {
-                var cubeIndex = j + 1;
-                var playedCardsName = playedCardsRootName + cubeIndex;
-                m_playedCardsGO [player, j] = GameObject.Find (playedCardsName);
-                if (m_playedCardsGO [player, j] == null)
-                    Debug.LogError ("Can't find PlayedCardsGO " + playedCardsName);
-                var playedCardsBackgroundName = playedCardsName + "/Background";
-                var playedCardsBackgroundGO = GameObject.Find (playedCardsBackgroundName);
-                if (playedCardsBackgroundGO == null)
-                    Debug.LogError ("Can't find PlayedCardsBackgroundGO " + playedCardsBackgroundName);
-                m_playedCardsBackground [player, j] = playedCardsBackgroundGO.GetComponent<Image> ();
-                var playedCardsValueName = playedCardsBackgroundName + "/Value";
-                var playedCardsValueGO = GameObject.Find (playedCardsValueName);
-                if (playedCardsValueGO == null)
-                    Debug.LogError ("Can't find PlayedCardsValueGO " + playedCardsValueName);
-                m_playedCardsValue [player, j] = playedCardsValueGO.GetComponent<Text> ();
-            }
-        }
-    }
-
     void Update ()
     {
     }
@@ -105,6 +65,43 @@ public class RaceUI : MonoBehaviour
     public void Initialise (GameLogic gamelogic)
     {
         m_gameLogic = gamelogic;
+
+        var raceCardName = "/" + name + "/RaceCard/";
+        var backgroundName = raceCardName + "Background";
+        var backgroundGO = GameObject.Find (backgroundName);
+        m_background = backgroundGO.GetComponent<Image> ();
+        if (m_background == null)
+            Debug.LogError ("Can't find Background " + backgroundName);
+        var cubeNamePrefix = backgroundName + "/";
+        for (int i = 0; i < NumberOfCubes; ++i) {
+            var cubeIndex = i + 1;
+            var cubeName = cubeNamePrefix + "Cube" + cubeIndex;
+            var cubeGO = GameObject.Find (cubeName);
+            m_cubeImages [i] = cubeGO.GetComponent<Image> ();
+            if (m_cubeImages [i] == null)
+                Debug.LogError ("Can't find Cube[" + cubeIndex + "] '" + cubeName + "'");
+        }
+
+        for (int player = 0; player < GameLogic.PlayerCount; ++player) {
+            var playedCardsRootName = raceCardName + (Player)player + "Card";
+            for (int j = 0; j < NumberOfCubes; ++j) {
+                var cubeIndex = j + 1;
+                var playedCardsName = playedCardsRootName + cubeIndex;
+                m_playedCardsGO [player, j] = GameObject.Find (playedCardsName);
+                if (m_playedCardsGO [player, j] == null)
+                    Debug.LogError ("Can't find PlayedCardsGO " + playedCardsName);
+                var playedCardsBackgroundName = playedCardsName + "/Background";
+                var playedCardsBackgroundGO = GameObject.Find (playedCardsBackgroundName);
+                if (playedCardsBackgroundGO == null)
+                    Debug.LogError ("Can't find PlayedCardsBackgroundGO " + playedCardsBackgroundName);
+                m_playedCardsBackground [player, j] = playedCardsBackgroundGO.GetComponent<Image> ();
+                var playedCardsValueName = playedCardsBackgroundName + "/Value";
+                var playedCardsValueGO = GameObject.Find (playedCardsValueName);
+                if (playedCardsValueGO == null)
+                    Debug.LogError ("Can't find PlayedCardsValueGO " + playedCardsValueName);
+                m_playedCardsValue [player, j] = playedCardsValueGO.GetComponent<Text> ();
+            }
+        }
     }
 
     public void SetPlayedCardToCard (int playerIndex, int cardIndex, Card card)
